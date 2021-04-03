d5eadcba1a7713c1cdca460abc20c8e1df72df3d
Dominator98 <kirkj@goatse.info>
2021-04-03 03:09:45 +0200

    now it is MUCH easier to setup memeflags

--- post.php
+++ post.php
@@ -1,5 +1,10 @@
 <?php
 $memeflag=false;
+$dst="flags";
+$postText = htmlspecialchars($_POST["message"]);
+if(!$postText){
+    exit("You forgot to input text!");
+}
 $postName = htmlspecialchars($_POST["name"]);
 $postFlag = htmlspecialchars($_POST["flag"]);
 $postNum = file_get_contents("counter.txt");
@@ -34,11 +39,31 @@ if(isset($_FILES['image'])){
     }
 }
 $ip = $_SERVER['REMOTE_ADDR'];
-$postText = htmlspecialchars($_POST["message"]);
-if(!$postText){
-    exit("You forgot to input text!");
-}
 $postTemplate = file_get_contents("template.html");
+$ast="memeflags/".$postFlag.".";
+if (file_exists($ast."gif") && file_exists($ast."txt")){
+    $dst = "memeflags";
+    $cc = $postFlag.".gif";
+    $cn = file_get_contents($ast."txt");
+}else{
+    if ($ip != "::1") {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "ip-api.com/json/$ip");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $info = curl_exec($ch);
+        curl_close($ch);
+        $jsonip = json_decode($info);
+        $cc = $jsonip->{"countryCode"};
+        $cn = $jsonip->{"country"};
+        $dst = "flags";
+        $cc .= ".gif";
+        $cc = strtolower($cc);
+    }else {
+        $cc = "lh.gif";
+        $cn = "Localhost";
+    }
+}
+/*
 switch($postFlag){
     case("fk"):
         $cc = "fs.gif";
@@ -81,32 +106,15 @@ switch($postFlag){
         $memeflag=true;
         break;
 }
-if(!$memeflag){
-    if ($ip != "::1") {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "ip-api.com/json/$ip");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $info = curl_exec($ch);
-        curl_close($ch);
-        $jsonip = json_decode($info);
-        $cc = $jsonip->{"countryCode"};
-        $cn = $jsonip->{"country"};
-        $cc .= ".gif";
-        $cc = strtolower($cc);
-    }else {
-        $cc = "lh.gif";
-        $cn = "Localhost";
-    }
-}
-if(!$postName){
-    $postName = "Anonymous";
-}
+*/
+if(!$postName) $postName = "Anonymous";
+
 file_put_contents("counter.txt", $postNum+1);
 $postHTML = str_replace("<_POSTNAME_>",$postName, $postTemplate);
 $postHTML = str_replace("<_POSTTEXT_>",$postText, $postHTML);
 $postHTML = str_replace("<_POSTNUM_>",$postNum, $postHTML);
 $postHTML = str_replace("<_POSTDATE_>",date("Y/m/d g:i:s"), $postHTML);
-$postHTML = str_replace("<_POSTFLAG_>","<img src='flags/$cc' title='$cn'>", $postHTML);
+$postHTML = str_replace("<_POSTFLAG_>","<img src='$dst/$cc' title='$cn'>", $postHTML);
 if ($fu==true){
     $wow = $postNum.".".$file_ext;
     $st = '<br><img width="100px" height="100px" style="float: left;" src="images/thingtochange"><br><br><br><br>';
